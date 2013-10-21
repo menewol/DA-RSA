40935bb3e2d18a8204d1c0c5027ec1d0be52da77
menewol <wolfgang@menezes.at>
2013-10-21 10:01:29 +0200

    added encryption sccheiss

--- DA-RSA/Form1.cs
+++ DA-RSA/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,15 +17,117 @@ namespace DA_RSA
     {
         int bitLength=1024;
         BigInteger N, E, D;
+        Thread _pThread, _qThread, _eThread;
 
         public void GenerateKeyPair()
-        { 
-        
+        {
+            Generator.Initialize(2);
+            BigInteger numMin = BigInteger.Pow(2, (bitLength / 2) - 1);
+            BigInteger numMax = BigInteger.Pow(2, (bitLength / 2));
+            var p = new PrimeNumber();
+            var q = new PrimeNumber();
+            p.SetNumber(Generator.Random(numMin, numMin));
+            q.SetNumber(Generator.Random(numMin, numMax));
+            _pThread = new Thread(p.RabinMiller);
+            _qThread = new Thread(q.RabinMiller);
+            DateTime start = DateTime.Now;
+            _pThread.Start();
+            _qThread.Start();
+            while (_pThread.IsAlive || _qThread.IsAlive)
+            {
+                Application.DoEvents();
+                TimeSpan ts = DateTime.Now - start;
+                if (ts.TotalMilliseconds > (1000 * 60 * 5))
+                {
+                    try
+                    {
+                        _pThread.Abort();
+                    }
+                    // ReSharper disable EmptyGeneralCatchClause
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message);
+                    }
+
+                    try
+                    {
+                        _qThread.Abort();
+                    }
+                    // ReSharper disable EmptyGeneralCatchClause
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message);
+                    }
+
+                    MessageBox.Show("Key generating error: timeout.\r\n\r\nIs your bit length too large?", "Error");
+
+                    break;
+                }
+            }
+            if (p.GetFoundPrime() && q.GetFoundPrime())
+            {
+                BigInteger n = p.GetPrimeNumber() * q.GetPrimeNumber();
+                BigInteger euler = (p.GetPrimeNumber() - 1) * (q.GetPrimeNumber() - 1);
+                var e = new PrimeNumber();
+                while (true)
+                {
+                    e.SetNumber(Generator.Random(2, euler - 1));
+                    start = DateTime.Now;
+                    _eThread = new Thread(e.RabinMiller);
+                    _eThread.Start();
+
+                    while (_eThread.IsAlive)
+                    {
+                        Application.DoEvents();
+                        TimeSpan ts = DateTime.Now - start;
+
+                        if (ts.TotalMilliseconds > (1000 * 60 * 5))
+                        {
+                            MessageBox.Show("Key generating error: timeout.\r\n\r\nIs your bit length too large?", "Error");
+                            break;
+                        }
+                    }
+
+                    if (e.GetFoundPrime() && (BigInteger.GreatestCommonDivisor(e.GetPrimeNumber(), euler) == 1))
+                    {
+                        break;
+                    }
+                }
+                if (e.GetFoundPrime())
+                {
+                    BigInteger d = MathExtended.ModularLinearEquationSolver(e.GetPrimeNumber(), 1, euler);
+                    if (d > 0)
+                    {
+                        // N
+                        N = n;
+                       
+                        // E
+                        E = e.GetPrimeNumber();
+
+                        // D
+                        D = d;
+
+                        MessageBox.Show("Successfully created key pair.");
+                    }
+                    else
+                    {
+                       MessageBox.Show("Error: Modular equation solver fault.");
+
+                        MessageBox.Show(
+                            "Error using mathematical extensions.\r\ne = " + e + "\r\neuler = " + euler + "\r\np = " +
+                            p.GetPrimeNumber() + "\r\n" + q.GetPrimeNumber(), "Error");
+                    }
+                }
+
+            }
+
         }
+
         public Form1()
         {
             InitializeComponent();
-            
+            GenerateKeyPair();
+            MessageBox.Show(N.ToString() + "\r\n" + E.ToString() + "\r\n" + D.ToString());
         }
     }
 }
