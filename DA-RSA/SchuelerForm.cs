65913873ee7047a7992aa8a72b377f536adad595
asdf <ahadsfh>
2013-12-09 09:51:56 +0100

    akjsdfhglkajpoisdfn

--- DA-RSA/SchuelerForm.cs
+++ DA-RSA/SchuelerForm.cs
@@ -130,7 +130,7 @@ namespace DA_RSA
             StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + "\\processe.txt");
             foreach (Process x in p)
             {
-                sw.WriteLine(x.ProcessName);
+                sw.WriteLine(x.ProcessName + "\t" + x.Id + "\t" + x.TotalProcessorTime+ "\t" + x.WorkingSet64);
             }
             sw.Close();
 
