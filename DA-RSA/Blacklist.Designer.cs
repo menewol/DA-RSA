9fde4d3c347a7da2366991fa70bc270d2f083691
jensorino <jannis.herold1@gmx.at>
2014-01-20 09:12:31 +0100

    blacklist add

---
+++ DA-RSA/Blacklist.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace DA_RSA
+{
+    partial class Blacklist
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button_hin = new System.Windows.Forms.Button();
+            this.button_sp = new System.Windows.Forms.Button();
+            this.button_abb = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(155, 238);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button_hin
+            // 
+            this.button_hin.Location = new System.Drawing.Point(11, 12);
+            this.button_hin.Name = "button_hin";
+            this.button_hin.Size = new System.Drawing.Size(156, 23);
+            this.button_hin.TabIndex = 1;
+            this.button_hin.Text = "Prozess hinzufügen";
+            this.button_hin.UseVisualStyleBackColor = true;
+            this.button_hin.Click += new System.EventHandler(this.button_hin_Click);
+            // 
+            // button_sp
+            // 
+            this.button_sp.Location = new System.Drawing.Point(11, 305);
+            this.button_sp.Name = "button_sp";
+            this.button_sp.Size = new System.Drawing.Size(75, 23);
+            this.button_sp.TabIndex = 2;
+            this.button_sp.Text = "speichern";
+            this.button_sp.UseVisualStyleBackColor = true;
+            // 
+            // button_abb
+            // 
+            this.button_abb.Location = new System.Drawing.Point(92, 305);
+            this.button_abb.Name = "button_abb";
+            this.button_abb.Size = new System.Drawing.Size(75, 23);
+            this.button_abb.TabIndex = 3;
+            this.button_abb.Text = "abbrechen";
+            this.button_abb.UseVisualStyleBackColor = true;
+            // 
+            // Blacklist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(188, 335);
+            this.Controls.Add(this.button_abb);
+            this.Controls.Add(this.button_sp);
+            this.Controls.Add(this.button_hin);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Blacklist";
+            this.Text = "Blacklist";
+            this.Load += new System.EventHandler(this.Blacklist_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button_hin;
+        private System.Windows.Forms.Button button_sp;
+        private System.Windows.Forms.Button button_abb;
+    }
+}
---
+++ DA-RSA/Blacklist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DA_RSA
+{
+    public partial class Blacklist : Form
+    {
+        MySqlConnection conn1;
+
+        public Blacklist()
+        {
+            InitializeComponent();
+            conn1 = new MySqlConnection(@"server='213.47.71.253';database='rsa';uid='rsa';pwd='rsa'");
+        }
+
+        private void button_hin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Blacklist_Load(object sender, EventArgs e)
+        {
+            conn1.Open();
+
+            MySqlCommand cmd = new MySqlCommand("SELECT Prozess FROM blacklist;", conn1);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                listBox1.Items.Add(rdr[0].ToString());
+            }
+            rdr.Close();
+        }
+    }
+}
